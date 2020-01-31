d6409c98af00517803cdfc1f10db5b3ee5d119bc
nerdyninjahugs <nerdyninjahugs4u@gmail.com>
2020-02-01 00:10:56 +0530

    Updates

---
+++ precache-manifest.0cd8173f877f28066436428c67400da9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ace92eb6dfd02f0dfb8d6a41d600b585",
+    "url": "/every_note/index.html"
+  },
+  {
+    "revision": "8d0057646d2eb8eebfc1",
+    "url": "/every_note/static/css/main.60d5545d.chunk.css"
+  },
+  {
+    "revision": "2a98aa462e708a2d1f51",
+    "url": "/every_note/static/js/2.1aeb34cc.chunk.js"
+  },
+  {
+    "revision": "2a196d222152c45027e5b2ccfe72e93e",
+    "url": "/every_note/static/js/2.1aeb34cc.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8d0057646d2eb8eebfc1",
+    "url": "/every_note/static/js/main.09bff230.chunk.js"
+  },
+  {
+    "revision": "30d8c36a07eb8b433351",
+    "url": "/every_note/static/js/runtime-main.799d28d8.js"
+  }
+]);
